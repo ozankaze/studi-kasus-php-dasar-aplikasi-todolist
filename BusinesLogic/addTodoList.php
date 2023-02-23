1790eeef4e9529b0264af2edc6fbf98263df654e
ozankaze <ozankaze101@gmail.com>
2023-02-23 19:43:56 +0700

    studi kasus php dasar - aplikasi todolist

--- BusinesLogic/addTodoList.php
+++ BusinesLogic/addTodoList.php
@@ -4,7 +4,7 @@
 function addTodoList(string $todo) {
     global $todoList;
 
-    $number = sizeof($todoList) + 1;
+    $number = count($todoList) + 1;
     
     $todoList[$number] = $todo;
 }
--- View/viewShowTodoList.php
+++ View/viewShowTodoList.php
@@ -1,6 +1,31 @@
 <?php
 
+require "../BusinesLogic/showTodoList.php";
+require "../BusinesLogic/addTodoList.php";
+require "../BusinesLogic/removeTodoList.php";
+require "../Helper/input.php";
 
 function viewShowTodolist() {
-    
+    while (true) {
+        showTodoList();
+
+        echo "MENU" . PHP_EOL;
+        echo "1. Tambah todo";
+        echo "2. Hapus todo";
+        echo "x. Keluar todo";
+
+        $pilihan = input("Pilih");
+
+        if ($pilihan == "1") {
+            viewAddTodoList();
+        } elseif ($pilihan == "2") {
+            viewRemoveTodoList();
+        } elseif ($pilihan == "x") {
+            // Keluar;
+            break;
+        } else {
+            echo "Data tidak di temukan";
+        }
+    }
+    echo "Sampai Jumpa Lagi" . PHP_EOL;
 }
