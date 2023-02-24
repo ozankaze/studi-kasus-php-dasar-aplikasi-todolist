657bd84bd9aa0ce2f077c19deac2bcfdada3fe02
ozankaze <ozankaze101@gmail.com>
2023-02-24 19:59:31 +0700

    studi kasus php dasar - aplikasi todolist

--- View/viewAddTodoList.php
+++ View/viewAddTodoList.php
@@ -1,6 +1,18 @@
 <?php
 
+require "../Helper/input.php";
+require "../BusinesLogic/addTodoList.php";
+// require "../Model/todoList.php";
+
 
 function viewAddTodoList() {
-    
+    echo "MENAMBAH TodoList";
+
+    $x = input("Todo (x untuk keluar): ");
+
+    if( $x == "x" ){
+        // cancel
+    } else {
+        addTodoList($x);
+    }
 }
