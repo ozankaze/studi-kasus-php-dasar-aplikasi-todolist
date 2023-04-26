c3011157b8e556c483518f51930021c183c2996c
ozankaze <ozankaze101@gmail.com>
2023-04-26 09:35:20 +0700

    Studi Kasus PHP OOP : Aplikasi TodoList Membuat View

---
+++ View/TodolistView.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace View
+{
+
+    class TodolistView
+    {
+
+        function showTodolist(): void
+        {
+            
+        }
+
+        function addTodolist(): void
+        {
+
+        }
+
+        function removeTodolist(): void
+        {
+
+        }
+
+    }
+
+}
